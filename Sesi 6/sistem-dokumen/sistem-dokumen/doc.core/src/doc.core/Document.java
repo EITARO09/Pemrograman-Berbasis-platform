de2377ba721e16ee64b32e0a2c3f674e826d2ffb
EITARO09 <muhzackymaulidin09@GMAIL.COM>
2025-10-23 19:27:33 +0700

    Add files via upload

---
+++ Sesi 6/sistem-dokumen/sistem-dokumen/doc.core/src/doc.core/Document.java
@@ -0,0 +1,11 @@
+package doc.core;
+
+public class Document {
+    private String title;
+    private String content;
+
+    public Document(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
